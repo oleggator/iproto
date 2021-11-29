0e404575c7913a6458fdabb6ff857942fab52e46
Oleg Utkin <oleg_utkin@me.com>
2021-11-30 02:12:54 +0300

    decode response code indicator

--- src/iproto/response.rs
+++ src/iproto/response.rs
@@ -4,21 +4,25 @@ use crate::iproto::consts;
 
 pub struct ResponseHeader {
     request_id: usize,
+    response_code_indicator: u32,
 }
+
+const RESPONSE_CODE_INDICATOR: u8 = 0x00;
 
 impl ResponseHeader {
     pub fn decode<R: Read>(reader: &mut R) -> Result<Self, rmp_serde::decode::Error> {
         let mut request_id: Option<usize> = None;
+        let mut response_code: Option<u32> = None;
 
         let map_len = rmp::decode::read_map_len(reader)?;
         for _ in 0..map_len {
             let code = rmp::decode::read_pfix(reader)?;
             match code {
+                RESPONSE_CODE_INDICATOR => {
+                    response_code = Some(rmp::decode::read_u32(reader)?);
+                }
                 consts::IPROTO_SYNC => {
                     request_id = Some(rmp::decode::read_u64(reader)? as usize);
-                }
-                consts::IPROTO_REQUEST_TYPE => {
-                    let _ = rmp::decode::read_u32(reader)?;
                 }
                 consts::IPROTO_SCHEMA_VERSION => {
                     rmp::decode::read_u32(reader)?;
@@ -31,12 +35,14 @@ impl ResponseHeader {
 
         Ok(ResponseHeader {
             request_id: request_id.unwrap(),
+            response_code_indicator: response_code.unwrap(),
         })
     }
 
     pub fn request_id(&self) -> usize {
         self.request_id
     }
+    pub fn response_code_indicator(&self) -> u32 { self.response_code_indicator }
 }
 
 pub trait Response<R: Read, B: DeserializeOwned>: Sized {
